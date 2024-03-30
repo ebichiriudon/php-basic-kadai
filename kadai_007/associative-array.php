e761b563d29169ff3621851470edc7d4dbb7a428
ebichiriudon <154717971+ebichiriudon@users.noreply.github.com>
2024-03-30 21:23:39 +0900

    kadai_007

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang=ja>
+    <head>
+      <meta charset="UTF-8">
+      <title>Kadai_007</title>
+    </head>
+    <body>
+      <p>
+         <?php
+        
+        $vegitables = ['name'=>'onion','price'=>'200','weight'=>'160'];
+
+        print_r($vegitables);
+
+
+
+         ?>
+      </p>
+
+</html>
